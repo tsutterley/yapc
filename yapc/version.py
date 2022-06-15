38883b7cbceeeb61b36318cf00d65dad1859fd44
tsutterley <tsutterl@uw.edu>
2022-06-14 17:11:16 -0700

    update project name (yapc taken on pypi)

--- yapc/version.py
+++ yapc/version.py
@@ -4,6 +4,6 @@ version.py (09/2021)
 """
 from pkg_resources import get_distribution
 # get version for distribution
-version = get_distribution("yapc").version
+version = get_distribution("pyYAPC").version
 # append "v" before the version
 full_version = "v{0}".format(version)
